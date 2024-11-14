127718c68e5adca618af0409d8b4402b313ab641
wiktorlemanski <wiktorlem1@wp.pl>
2024-11-14 13:28:07 +0100

    TODO updated

--- app/components/EditTodoForm.tsx
+++ app/components/EditTodoForm.tsx
@@ -1,7 +1,7 @@
 /*
 *TODO:
-* 1. Refactor useState for fields of todos
-* 2. finish error logic
+* 1. Refactor useState for fields of todos -DONE
+* 2. finish error logic - STARTED
 * 3. Change styles and make it in modules
 * 4  Create layout with header and footer for pages
 *   4.1 header must contain links with react router for form and todos list
@@ -14,45 +14,81 @@
 
 import IEditTodoFormProps from "@/app/interfaces/IEditTodoFormProps";
 import {useState} from "react";
+import ITodo from "@/app/interfaces/IToDo";
 
 
 export default function EditTodoForm({todo, onSave, onCancel, onClose}:IEditTodoFormProps) {
-    const [name, setName] = useState<string>(todo.name);
-    const [description, setDescription] = useState<string>(todo.description);
-    const [startDate, setStartDate] = useState<string>(
-        todo.startDate ? todo.startDate.toISOString().substring(0, 10) : ''
-    );
-    const [endDate, setEndDate] = useState<string>(
-        todo.endDate ? todo.endDate.toISOString().substring(0, 10) : ''
-    );
-    const [category, setCategory] = useState<string>(todo.category);
+
+
+
+    const [formData, setFormData] = useState({
+        name:todo.name,
+        description: todo.description,
+        startDate: todo.startDate ? todo.startDate.toISOString().substring(0, 10) : '',
+        endDate: todo.endDate ? todo.endDate.toISOString().substring(0, 10) : '',
+        category: todo.category
+    })
+
     const [errors, setErrors] = useState<{[key:string] :string}>({})
 
     const handleSave = () => {
-        const updatedTodo = {
-            ...todo,
-            name,
-            startDate: startDate ? new Date(startDate) : null,
-            endDate: startDate ?  new Date(endDate) : null,
-            description,
-            category
-        };
-        onSave(updatedTodo);
-    };
-    const handleValidate = () =>{
+        if (handleValidate()){
+            const updatedTodo :ITodo = {
+                ...todo,
+                name: formData.name,
+                startDate: formData.startDate ? new Date(formData.startDate) : null,
+                endDate: formData.startDate ?  new Date(formData.endDate) : null,
+                description: formData.description,
+                category: formData.category
+            };
+            onSave(updatedTodo);
+        }
 
+    };
+
+    const handleChange = (field: string, value: string) =>{
+        setFormData(prevState =>({
+            ...prevState,
+            [field] :value
+        }))
+    }
+    const handleValidate = () =>{
+            const newErrors:{[key:string] :string} = {}
+        if (formData.name.length<4){
+            newErrors.name = 'za krotka nazwa zadania!'
+            console.log(newErrors.name)
+        }
+
+        setErrors(newErrors)
+        return Object.keys(newErrors) === 0
     }
 
 
     return (
         <div className={'edit-todo-form'}>
-            <input value={name} onChange={(e) => setName(e.target.value)}
-            placeholder={'nazwa zadania'}/>
-            <textarea value={description} onChange={(e) => setDescription(e.target.value)}
-            placeholder={'opis'}/>
-            <input value={startDate} onChange={(e) => setStartDate(e.target.value)}/>
-            <input value={endDate} onChange={(e) => setEndDate(e.target.value)}/>
-            <input value={category} onChange={(e) => setCategory(e.target.value)}/>
+            <input
+                value={formData.name}
+                onChange={(e)=>handleChange("name", e.target.value)}
+            />
+            {errors.name && <p className={'error-p'}>{errors.name}</p>}
+            <textarea
+                value={formData.description}
+                onChange={(e) => handleChange('description', e.target.value)}
+
+            />
+            <input value={formData.startDate}
+                   onChange={
+                (e)=>handleChange('startDate', e.target.value)}/>
+            <input value={formData.endDate}
+                   onChange={
+                       (e)=>handleChange('endDate', e.target.value)}
+
+            />
+            <input
+                value={formData.category}
+                onChange={
+                (e) =>handleChange('category', e.target.value)}
+            />
             <button onClick={handleSave}>zapisz</button>
             <button onClick={onCancel}>anuluj</button>
             <button onClick={onClose} onSubmit={handleValidate}>[zamknij]</button>
