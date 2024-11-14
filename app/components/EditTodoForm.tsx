d26882b1462e607dd6bc7420ef484b8a904bb9b5
wiktor <wiktorlem1@wp.pl>
2024-11-14 03:29:48 +0100

    things to do in project added at the top of the EditTodoForm component

--- app/components/EditTodoForm.tsx
+++ app/components/EditTodoForm.tsx
@@ -1,3 +1,16 @@
+/*
+*TODO:
+* 1. Refactor useState for fields of todos
+* 2. finish error logic
+* 3. Change styles and make it in modules
+* 4  Create layout with header and footer for pages
+*   4.1 header must contain links with react router for form and todos list
+* 5 save todos in local storage
+*
+*
+*
+*  */
+
 import IEditTodoFormProps from "@/app/interfaces/IEditTodoFormProps";
 import {useState} from "react";
 
